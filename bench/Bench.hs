e20368b76f99b97c180973031166d3a6547fe9ca
evansb <evans@comp.nus.edu.sg>
2014-12-02 08:56:40 +0800

    Change to pure benchmark

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -5,16 +5,12 @@ import Control.Monad
 import System.IO
 import System.Directory
 import Criterion.Main
-import Language.Java.Lexer
-import Language.Java.AST
 
-runLexer :: String -> IO ()
-runLexer str = writeFile "/dev/null" (show (lexJava str))
+import Language.Java.Lexer
+import Language.Java.Parser
 
 headerStr :: String -> String -> String
-headerStr file content = "File : "
-                        ++ file ++ " of "
-                        ++ show (length content)
+headerStr file content = "File : " ++ file ++ " of " ++ show (length content)
                         ++ " characters."
 
 notJava :: String -> Bool
@@ -22,12 +18,13 @@ notJava "." = True
 notJava str = (length str - length ".java") < 0 ||
             (drop (length str - length ".java") str /= ".java")
 
-
 benchFile :: String -> IO ()
 benchFile file = unless (notJava file) $ do
         putStrLn ("Benchmarking " ++ file)
         content <- readFile ("./java-src/" ++ file)
-        defaultMain [bench (headerStr file content) $ nfIO (runLexer content)]
+        defaultMain [bgroup "lexer"  [bench file $ whnf lexJava content]]
+        let tokens = lexJava content
+        defaultMain [bgroup "parser" [bench file $ whnf parseTokens tokens]]
 
 main :: IO ()
 main = getDirectoryContents "java-src/" >>= mapM_ benchFile
