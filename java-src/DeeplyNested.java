98872c32f4390cfe530c5e5be470301ded063d9e
evansb <evans@comp.nus.edu.sg>
2014-12-02 15:37:40 +0800

    Add the benchmark file

---
+++ java-src/DeeplyNested.java
@@ -0,0 +1,36 @@
+
+public class DeeplyNested {
+    class Deep2 {
+        public void Deep3() {
+            {
+                while (true) {
+                    while (true) {
+                        while (true)  {
+                            while (true)  {
+                                while (true)  {
+                                    while (true)  {
+                                        while (true) {
+                                            while (true) {
+                                                while (true)  {
+                                                    while (true)  {
+                                                        while (true)  {
+                                                            while (true)  {
+
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+        {{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
+    }
+}
